079a9d5b398c96caf1211fd37936172f7bbfcb45
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2022-12-28 23:30:48 +0200

    in progress

--- src/js/hide-scroll-up-button.js
+++ src/js/hide-scroll-up-button.js
@@ -6,9 +6,9 @@ function hideScrollUpBtn() {
   const scrollUpButton = document.querySelector('.scroll-up-btn');
 
   if (scroll > 300) {
-    scrollUpButton.classList.remove('scroll-up-btn--hidden');
+    scrollUpButton.classList.remove('scroll-up-btn--show');
     return;
   }
-  scrollUpButton.classList.add('scroll-up-btn--hidden');
   scrollUpButton.blur();
+  scrollUpButton.classList.add('scroll-up-btn--show');
 }
--- src/js/showTrending/renderTrending.js
+++ src/js/showTrending/renderTrending.js
@@ -2,12 +2,14 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 import { fetchTrending } from './fetchTrending';
+import axios from 'axios';
 
 const refs = {
   searchForm: document.querySelector('.searchForm'),
   searchQueryInput: document.querySelector('input[name=searchQuery]'),
   searchBtn: document.querySelector('.searchForm__button'),
   gallery: document.querySelector('.gallery'),
+  backdrop: document.querySelector('.backdrop'),
 };
 
 let page = 1;
@@ -24,7 +26,6 @@ const renderMarkup = async () => {
       Loading.hourglass();
 
       refs.gallery.innerHTML = galleryMarkupСreation(results);
-
       if (total_results > pageLimit) {
         observeLastCard();
       }
@@ -45,17 +46,16 @@ renderMarkup();
 const galleryMarkupСreation = results => {
   const markup = results
     .map(
-      ({ poster_path, title, release_date }) => `
-        <li class="movieCard" data-modal-open>
-            <article>
-                <a>
-                    <img class="movieCard__image" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="movieImg" />
-                    <div class="movieCard__info">
-                        <p class="movieCard__title">${title}</p>
-                        <p class="movieCard__description">Drama, Action | ${release_date}</p>
-                    </div>
-                </a>
-            </article>
+      ({ poster_path, title, id, release_date }) => `
+        <li class="movieCard">
+          <a data-id=${id}>
+            <img class="movieCard__image" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="movieImg" />              
+                <p class="movieCard__info movieCard__title">${title}</p>
+                <p class="movieCard__info movieCard__description">Drama, Action | ${release_date.substr(
+                  0,
+                  4
+                )}</p>            
+          </a> 
         </li>
       `
     )
@@ -68,7 +68,6 @@ const loadMore = async () => {
   Loading.hourglass();
 
   try {
-    console.log(page);
     const { results, total_pages, total_results } = await fetchTrending(page);
     refs.gallery.insertAdjacentHTML(
       'beforeend',
@@ -101,4 +100,122 @@ const observer = new IntersectionObserver(
 const observeLastCard = () => {
   lastCard = document.querySelector('.movieCard:last-child');
   observer.observe(lastCard);
+};
+
+const onMovieCardClick = async e => {
+  event.preventDefault();
+  const id = e.target.parentElement.dataset.id;
+
+  try {
+    const resp = await fetchMovieForModal(id);
+    refs.backdrop.innerHTML = modalMarkupСreation(resp);
+    refs.backdrop.classList.remove('backdrop--hidden');
+    // console.log(resp);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error.message);
+  }
+};
+
+refs.gallery.addEventListener('click', onMovieCardClick);
+
+const fetchMovieForModal = async id => {
+  const mediaType = 'movie';
+  const URL = 'https://api.themoviedb.org/3/';
+
+  let searchParams = new URLSearchParams({
+    api_key: 'ac91775ba29254b7e75060011bf34a90',
+  });
+
+  try {
+    const { data } = await axios.get(
+      `${URL}${mediaType}/${id}?${searchParams}`
+    );
+    return data;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error.message);
+  }
+};
+
+const modalMarkupСreation = ({
+  title,
+  vote_average,
+  vote_count,
+  popularity,
+  original_title,
+  genres,
+  overview,
+  poster_path,
+}) => {
+  const markup = `
+  <div class="movieModal">
+    <button class="button modal-close-button" type="button" data-modal-close>
+      <img
+        class="modal-close-button__cross"
+        src=""
+        alt="movieImg"
+      />
+    </button>
+    <img class="movieModal__image" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="movieImg" />
+    <div class="movieModal__info">
+      <table class="movieModal__table">
+        <caption class="movieModal__caption">
+          ${title}
+        </caption>
+        <tbody>
+          <tr>
+            <td class="movieModal__parameter padding-bottom-td">
+              Vote / Votes
+            </td>
+            <td class="movieModal__value padding-bottom-td">
+              <span class="movieModal__vote">${vote_average}</span> /
+              <span class="movieModal__votes">${vote_count}</span>
+            </td>
+          </tr>
+          <tr>
+            <td class="movieModal__parameter padding-bottom-td">
+              Popularity
+            </td>
+            <td class="movieModal__value padding-bottom-td">${popularity}</td>
+          </tr>
+          <tr>
+            <td class="movieModal__parameter padding-bottom-td">
+              Original Title
+            </td>
+            <td class="movieModal__value padding-bottom-td">
+              ${original_title}
+            </td>
+          </tr>
+          <tr>
+            <td class="movieModal__parameter no-padding-td">Genre</td>
+            <td class="movieModal__value no-padding-td">${genres}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="movieModal__description">
+        <p class="movieModal__about">about</p>
+        <p class="movieModal__text">
+          ${overview}
+        </p>
+      </div>
+      <div class="movieModal__btns">
+        <button
+          class="filmoteca-btn filmoteca-btn--primary"
+          type="button"
+          data-modal-close
+        >
+          add to Watched
+        </button>
+        <button
+          class="filmoteca-btn filmoteca-btn--secondary"
+          type="button"
+          data-modal-close
+        >
+          add to queue
+        </button>
+      </div>
+    </div>
+  </div>`;
+  return markup;
 };
