efcd53ee8d2595234594185de964e08e888d2ca0
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2023-01-03 11:49:46 +0200

    auth in progress

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { smoothscroll } from './js/smoothscroll';
 import { renderMarkup } from './js/showTrending/renderTrending';
 import { hiddenElementsOnMobileVersion } from './js/hideElementsInMobileVersion';
 import './js/firebase';
+import './js/signInModal';
 smoothscroll();
 window.addEventListener('scroll', hideScrollUpBtn);
 
 console.log('d/filmoteka');
+console.log('hello from heare');
--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { closeModalFunc } from './signInModal';
 
 import {
   getAuth,
@@ -10,6 +11,8 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   signInWithRedirect,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
   connectAuthEmulator,
 } from 'firebase/auth';
 
@@ -52,26 +55,93 @@ const auth = getAuth(firebaseApp);
 
 // ===============================================================
 
-signInWithEmailAndPassword(auth, email, password)
-  .then(userCredential => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-  })
-  .catch(error => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-
 const refs = {
+  backdrop: document.querySelector('[data-signInModal]'),
+  btnSignIn: document.querySelector('#signInModalOpen'),
+  btnLogOut: document.querySelector('#logoutBtn'),
+  btnGoogleLogin: document.querySelector('#googleLoginBtn'),
+  btnLoginWithEmail: document.querySelector('#loginWithEmailBtn'),
+  btnLoginEmail: document.querySelector('#loginEmailBtn'),
+
+  backdrop: document.querySelector('[data-signInModal]'),
+  navigation: document.querySelector('.navigation__list'),
+  boxUser: document.querySelector('.user__box'),
+  boxSignInModal: document.querySelector('.signInModal__box'),
+  boxSignInWithEmailModal: document.querySelector(
+    '.signInModal__signInWithEmail'
+  ),
+
   loginEmail: document.querySelector('#email'),
   loginPassword: document.querySelector('#password'),
-  btnLogin: document.querySelector('#loginBtn'),
-  btnGoogleLogin: document.querySelector('#googleLoginBtn'),
+
   btnSignUp: document.querySelector('#SignUpBtn'),
-  btnlogOut: document.querySelector('#logOut'),
+  userName: document.querySelector('.user__name'),
   formField: document.querySelector('.auth-form__field'),
   formTitle: document.querySelector('.auth-form__title'),
+};
+
+// monitorAuthState auth =====================================================================================
+
+const monitorAuthState = async () => {
+  try {
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        console.log(user);
+
+        switch (true) {
+          case user.displayName !== null && user.photoURL !== null:
+            refs.boxUser.innerHTML = `<img class="user__img" src="${user.photoURL}" alt="" />
+                                  <p class="user__greeting">Good to see You again</p>
+                                  <p class="user__name"> ${user.displayName}</p>`;
+            break;
+
+          case user.displayName === null && user.photoURL === null:
+            refs.boxUser.innerHTML = `<p class="user__greeting">Good to see You again</p>
+                                  <p class="user__name">Logged in as ${user.email}</p>`;
+            break;
+
+          default:
+            break;
+        }
+        refs.navigation.innerHTML = `<li class="navigation__item">
+                                      <a class="navigation__link navigation__link--current" href="">HOME</a>
+                                      </li>
+                                      <li class="navigation__item">
+                                      <a class="navigation__link" href="">MY LIBRARY</a>
+                                      </li>`;
+        refs.btnSignIn.classList.add('visually-hidden');
+        refs.btnLogOut.classList.remove('visually-hidden');
+        refs.backdrop.classList.add('backdrop--hidden');
+        refs.btnLogOut.addEventListener('click', logOut);
+        refs.btnSignIn.removeEventListener('click', onbtnSignInClick);
+      } else {
+        refs.boxUser.innerHTML = `<p class="user__name">Hello Stranger</p>`;
+        refs.navigation.innerHTML = ``;
+        refs.btnSignIn.classList.remove('visually-hidden');
+        refs.btnLogOut.classList.add('visually-hidden');
+        refs.btnLogOut.removeEventListener('click', logOut);
+        refs.btnSignIn.addEventListener('click', onbtnSignInClick);
+      }
+    });
+  } catch (error) {
+    showLoginError(error);
+  }
+};
+
+monitorAuthState();
+
+const onbtnSignInClick = e => {
+  event.preventDefault();
+  refs.backdrop.classList.remove('backdrop--hidden');
+  refs.btnGoogleLogin.addEventListener('click', onBtnGoogleLoginClick);
+  refs.btnLoginWithEmail.addEventListener('click', onbtnLoginWithEmailClick);
+  console.log('hello');
+  console.log(refs.btnLoginWithEmail);
+  closeModalFunc();
+};
+
+const logOut = async () => {
+  await signOut(auth);
 };
 
 const showLoginError = error => {
@@ -85,6 +155,25 @@ const showLoginError = error => {
   }
 };
 
+// signInWithEmailAndPassword auth =====================================================================================
+
+const onbtnLoginWithEmailClick = () => {
+  refs.btnLoginEmail.addEventListener('click', loginEmailPasspord);
+  refs.boxSignInWithEmailModal.classList.remove('visually-hidden');
+  refs.boxSignInModal.classList.add('visually-hidden');
+};
+
+signInWithEmailAndPassword(auth, email, password)
+  .then(userCredential => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch(error => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
 const loginEmailPasspord = async e => {
   e.preventDefault();
   const email = refs.loginEmail.value;
@@ -96,61 +185,32 @@ const loginEmailPasspord = async e => {
       email,
       password
     );
-
-    // console.log(userCredential.user);
   } catch (error) {
     showLoginError(error);
   }
 };
 
-refs.btnLogin.addEventListener('click', loginEmailPasspord);
+// createAccount  =====================================================================================
 
-const createAccount = async e => {
-  e.preventDefault();
-  const email = refs.loginEmail.value;
-  const password = refs.loginPassword.value;
+// const createAccount = async e => {
+//   e.preventDefault();
+//   const email = refs.loginEmail.value;
+//   const password = refs.loginPassword.value;
 
-  try {
-    const userCredential = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
+//   try {
+//     const userCredential = await createUserWithEmailAndPassword(
+//       auth,
+//       email,
+//       password
+//     );
 
-    // console.log(userCredential.user);
-  } catch (error) {
-    showLoginError(error);
-  }
-};
+//     // console.log(userCredential.user);
+//   } catch (error) {
+//     showLoginError(error);
+//   }
+// };
 
-refs.btnSignUp.addEventListener('click', createAccount);
-
-const monitorAuthState = async () => {
-  onAuthStateChanged(auth, user => {
-    if (user) {
-      //   console.log(user);
-      refs.formField.classList.add('visually-hidden');
-      refs.btnSignUp.classList.add('visually-hidden');
-      refs.btnLogin.classList.add('visually-hidden');
-      refs.btnGoogleLogin.classList.add('visually-hidden');
-      refs.btnlogOut.classList.remove('visually-hidden');
-
-      refs.formTitle.innerHTML = `Logged in as: ${user.email}`;
-    } else {
-      refs.btnSignUp.classList.remove('visually-hidden');
-      refs.btnLogin.classList.remove('visually-hidden');
-      refs.btnlogOut.classList.add('visually-hidden');
-    }
-  });
-};
-
-monitorAuthState();
-
-const logOut = async () => {
-  await signOut(auth);
-};
-
-refs.btnlogOut.addEventListener('click', logOut);
+// refs.btnSignUp.addEventListener('click', createAccount);
 
 // google auth =====================================================================================
 
@@ -162,7 +222,7 @@ provider.setCustomParameters({
   login_hint: 'user@example.com',
 });
 
-refs.btnGoogleLogin.addEventListener('click', e => {
+export const onBtnGoogleLoginClick = e => {
   e.preventDefault();
   signInWithPopup(auth, provider)
     .then(result => {
@@ -183,4 +243,35 @@ refs.btnGoogleLogin.addEventListener('click', e => {
       const credential = GoogleAuthProvider.credentialFromError(error);
       // ...
     });
-});
+};
+
+// // recaptcha auth =====================================================================================
+
+// window.recaptchaVerifier = new RecaptchaVerifier(
+//   refs.btnLogin,
+//   {
+//     size: 'invisible',
+//     callback: response => {
+//       // reCAPTCHA solved, allow signInWithPhoneNumber.
+//       onSignInSubmit();
+//     },
+//   },
+//   auth
+// );
+
+// // signInWithPhoneNumber auth =====================================================================================
+
+// const phoneNumber = getPhoneNumberFromUserInput();
+// const appVerifier = window.recaptchaVerifier;
+
+// signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+//   .then(confirmationResult => {
+//     // SMS sent. Prompt user to type the code from the message, then sign the
+//     // user in with confirmationResult.confirm(code).
+//     window.confirmationResult = confirmationResult;
+//     // ...
+//   })
+//   .catch(error => {
+//     // Error; SMS not sent
+//     // ...
+//   });
